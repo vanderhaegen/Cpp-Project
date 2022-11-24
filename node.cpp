bbda225dd290dc07d5f4e5c46646061dc24f7f1e
Henrique <henrique.vanderhaegen@gmail.com>
2022-11-23 23:28:53 -0300

    Adicionando arquivo nodo.cpp

---
+++ node.cpp
@@ -0,0 +1,36 @@
+#pragma once
+#include <iostream>
+#include "Nodo.hpp"
+
+using namespace std;
+
+// Construtor 1
+Nodo::Nodo (){
+	esquerda = direita = 0;
+}
+// Construtor 2
+Nodo::Nodo(double valor, Nodo *esq, Nodo *dir){
+	valor = val;
+	esquerda = esq;
+	direita = dir;
+}
+// Retorna valor do nodo
+double Nodo::GetValor (){
+	return valor;
+}
+// Altera valor
+void Nodo::setValor (double novoDado){
+	dado = novoDado;
+}
+// Ponteiro da esquerda
+Nodo* Nodo::nodoEsquerda(){
+	return esquerda;
+}
+// Ponteiro da direita
+Nodo* Nodo::nodoDireita (){
+	return direita;
+}
+// Verifica se o nodo está vazio
+bool Nodo::Vazio (){
+	return (esquerda == 0) && (direita == 0);
+}
