74aa2ec3a605e411e0fd25ef1d97077bdeb36cdc
Henrique <henrique.vanderhaegen@gmail.com>
2022-11-29 14:55:43 -0300

    Criando o menu

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+void print_menu(){
+    cout << "Menu" << endl;
+    cout << "Selecione uma opção:" << endl;
+    cout << "1) Visualizar dados" << endl;
+    cout << "2) asdfasdfasdf" << endl;
+    cout << "3) Encerrar programa" << endl;
+    }
+
+int main(){
+    int var=0;
+    while(var!=3){
+        print_menu();
+        cout << endl;
+        cin >> var;
+        cout << endl;
+        if(var==1){
+            cout << "Visualizando dados" << endl;
+        }
+        else if(var==2){
+            cout << "asdfasdfasdf" << endl;
+        }
+        if(var!=3){
+            cout << endl;
+            cout << "-----//-----//-----//-----//-----" << endl;
+            cout << endl;
+        }
+    }
+    cout << "Encerrando o programa";
+}
